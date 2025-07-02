18dd56a2474bfe673aade91db25c003f1329e47d
nguyenhien8585 <nguyenhien21022020@gmail.com>
2025-07-02 10:40:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from docx import Document
 from google.cloud import vision
 
 # Set your Google Vision service account JSON path
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "YOUR_GG_JSON_KEY.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gen-lang-client-0798870399-03afc399d966.json"
 
 st.set_page_config(layout="wide", page_title="PDF/Ảnh → LaTeX/Word + Auto crop minh họa (GPT-4o + GG Vision)")
 
