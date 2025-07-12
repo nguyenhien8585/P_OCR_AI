3c457000254e904904c0ef90948bfcb9e2bf66e7
nguyenhien8585 <nguyenhien21022020@gmail.com>
2025-07-12 10:05:28 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+API_URL = "https://script.google.com/macros/s/AKfycby6GUWKFttjWTDJuQuX5IAeGAzS5tQULLja3SHbSfZIhQyaWVMuxyRNAE-fykxnznkqIw/exec"
+API_KEY = "sk_nguyenhien21022020_pro_mcwzovbjz11wklh8zk"   # <-- Thay bằng key thật của bạn
