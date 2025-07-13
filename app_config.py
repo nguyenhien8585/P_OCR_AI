4ff233ad347a12921c214d38178353ee58700a81
nguyenhien8585 <nguyenhien21022020@gmail.com>
2025-07-13 10:40:00 +0700

    Delete app_config.py

--- app_config.py
+++
@@ -1,4 +0,0 @@
-OCR_API_URL = "https://script.google.com/macros/s/AKfycby6GUWKFttjWTDJuQuX5IAeGAzS5tQULLja3SHbSfZIhQyaWVMuxyRNAE-fykxnznkqIw/exec"
-OCR_API_KEY = "sk_nguyenhien21022020_pro_mcwzovbjz11wklh8zk"
-GPT4O_API_URL = "https://api.sv2.llm.ai.vn/v1"
-GPT4O_API_KEY = "sk-j4DkzI7htsVqEZqC272d3b58B0Fb49A183573dD2Fc04F71d"
