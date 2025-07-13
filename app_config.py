e728013238f1213647033916296f716ff1afee45
nguyenhien8585 <nguyenhien21022020@gmail.com>
2025-07-13 10:09:07 +0700

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,3 +1,2 @@
-# config.py
-API_URL = "https://script.google.com/macros/s/AKfycby6GUWKFttjWTDJuQuX5IAeGAzS5tQULLja3SHbSfZIhQyaWVMuxyRNAE-fykxnznkqIw/exec"
-API_KEY = "sk_nguyenhien21022020_pro_mcwzovbjz11wklh8zk"   # <-- Thay bằng key thật của bạn
+API_URL = "https://api.sv2.llm.ai.vn/v1"
+API_KEY = "sk-j4DkzI7htsVqEZqC272d3b58B0Fb49A183573dD2Fc04F71d" 
